2c18ba6d249cebdb2ef488334682da73e027b211
yjw <844478823@qq.com>
2017-03-16 14:18:44 +0800

    JoinUs1.0 change

---
+++ JoninUs/frontend/models/User_Person.php
@@ -0,0 +1,55 @@
+<?php
+	
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class User_Person extends Model{
+
+	public $person_email;
+	public $person_password;
+
+	public static function tableName()
+    {
+        return '{{%person}}';
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'person_email' => 'email',
+            'person_password' => 'password',
+        ];
+    }
+
+	public function create($data){//DAO模式
+        $sql = "insert into yii_person(person_email,person_password)values('{$data['email']}','{$data['password']}')";
+        return yii::$app->db->createCommand($sql)->execute();
+    }
+
+    public function getOne1($data)
+    {
+        $sql = "select * from yii_person where person_email='".$data['email']."' and 1=1 or 0=1";
+        return yii::$app->db->createCommand($sql)->queryOne();
+    }
+
+    public function getOne2($data)
+    {
+        $sql = "select * from yii_company where company_email='".$data['email']."' and 1=1 or 0=1";
+        return yii::$app->db->createCommand($sql)->queryOne();
+    }
+
+    public function getOne3($data)
+    {
+        $sql = "select person_password from yii_person where person_email='".$data['email']."' and 1=1 or 0=1";
+        return yii::$app->db->createCommand($sql)->queryOne();
+    }
+
+    public function getOne4($data)
+    {
+        $sql = "select company_password from yii_company where company_email='".$data['email']."' and 1=1 or 0=1";
+        return yii::$app->db->createCommand($sql)->queryOne();
+    }
+
+}
